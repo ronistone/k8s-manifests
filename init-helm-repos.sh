7e765fbd268116504f909cd6f57eaecea8966a8e
Ronistone Junior <ronistonejunior@gmail.com>
2021-02-21 12:03:49 -0300

    initing repository

---
+++ init-helm-repos.sh
@@ -0,0 +1,4 @@
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
